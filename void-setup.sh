a65f6c2a16f6c15e1ed458213cb5597136ddd2be
Force A. Gamer <forcegamer159@gmail.com>
2023-01-05 18:12:17 +0100

    Changed Rofi mode to drun

--- void-setup.sh
+++ void-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -e "\nINSTALLING PREREQUISITES\n"
+
 # Prerequisites
 xbps-install \
     curl \
@@ -7,13 +9,19 @@ xbps-install \
     git \
 -y
 
+echo -e "\nINSTALLING FLATPAK\n"
+
 # Flatpak installation
 xbps-install flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+echo -e "\nINSTALLING INTERNET\n"
+
 # Internet
 flatpak install flathub com.discordapp.Discord -y
 xbps-install qbittorrent -y
+
+echo -e "\nINSTALLING CREATIVE\n"
 
 # Creative
 xbps-install \
@@ -23,6 +31,8 @@ xbps-install \
     obs \
 -y
 
+echo -e "\nINSTALLING GAMING\n"
+
 # Gaming
 xbps-install \
     steam \
@@ -31,6 +41,8 @@ xbps-install \
     PrismLauncher \
     wine-devel \
 -y
+
+echo -e "\nINSTALLING PRODUCTIVITY\n"
 
 # Productivity
 xbps-install \
@@ -50,6 +62,8 @@ curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install
 #     linux-headers-liquorix-amd64
 # -y
 
+echo -e "\nINSTALLING WINDOW MANAGER TOOLS\n"
+
 # Window Manager tools
 xbps-install \
     bspwm \
@@ -58,10 +72,12 @@ xbps-install \
     polybar \
     dunst \
     rofi \
-    thunar \
+    Thunar \
     spectacle \
     picom \
 -y
+
+echo -e "\nINSTALLING FONTS\n"
 
 # Fonts
 xbps-install \
@@ -70,4 +86,20 @@ xbps-install \
     font-material-design-icons-ttf \
 -y
 
-echo "\e[32Setup complete!\e[0m"
+echo -e "\nSETTING UP CONFIGS\n"
+
+# Config setup
+mkdir ~/.config/bspwm
+mkdir ~/.config/sxhkd
+mkdir ~/.config/polybar
+
+cd ./configs
+cp ./bspwmrc ~/.config/bspwm/bspwmrc
+cp ./sxhkdrc ~/.config/sxhkd/sxhkdrc
+cp ./config.ini ~/.config/polybar/config.ini
+cp ./polybar_screens.sh ~/.config/polybar_screens.sh
+
+chmod +x ~/.config/bspwm/bspwmrc
+chmod +x ~/.config/sxhkd/sxhkdrc
+
+echo -e "\nSETUP COMPLETE\n"
